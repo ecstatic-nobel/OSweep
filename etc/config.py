54b1299153e16607ea0e7c58e96cc0a9812d74c3
ecstatic-nobel <theemailaddressishare@protonmail.com>
2018-11-03 20:17:57 -0400

    Create new directory for config.py. Move file. Prep file for Censys and Twitter API.

---
+++ etc/config.py
@@ -0,0 +1,25 @@
+#!/opt/splunk/bin/python
+
+#
+# OSweep Configurations
+#
+
+# Censys API
+censys_uid    = None
+censys_secret = None
+
+# Cymon API
+cymon_api_key = None
+
+# GreyNoise API
+gn_api_key = None
+
+# Hybrid-Analysis API
+ha_api_key    = None
+ha_secret_key = None
+
+# Twitter API
+twitter_consumer_key        = None
+twitter_consumer_secret     = None
+twitter_access_token        = None
+twitter_access_token_secret = None
