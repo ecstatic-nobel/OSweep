cac78b54908688359948215cea6a7a0b8da3311c
ecstatic-nobel <theemailaddressishare@protonmail.com>
2018-11-17 00:19:47 -0500

    Add Pulsedive

--- etc/config.py
+++ etc/config.py
@@ -11,6 +11,9 @@ https_proxy_url = None
 # GreyNoise API
 greynoise_key = None
 
+# Pulsedive API
+pulsedive_apikey = None
+
 # Twitter API
 twitter_consumer_key        = None
 twitter_consumer_secret     = None
