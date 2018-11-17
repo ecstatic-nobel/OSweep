acb1d5ee85621cc052f920034c1b4907ca938c80
ecstatic-nobel <theemailaddressishare@protonmail.com>
2018-11-17 14:43:49 -0500

    Remove unneeded modules. Reformat. Remove unneeded code from write_file function.

--- bin/pulsedive.py
+++ bin/pulsedive.py
@@ -3,14 +3,12 @@
 
 """
 
-# from collections import OrderedDict
 import os
 import sys
 import time
 
 script_path = os.path.dirname(os.path.realpath(__file__)) + "/_tp_modules"
 sys.path.insert(0, script_path)
-# import validators
 
 import commons
 
@@ -52,24 +50,28 @@ def query_list(session):
             continue
 
         parse_resp = resp.json()
-        feed_dict        = {}
-        feed_dict["fid"] = str(parse_resp.get("fid", ""))
+        feed_dict           = {}
+        feed_dict["source"] = "feed"
+        feed_dict["fid"]    = str(parse_resp.get("fid", ""))
 
         if feed_dict["fid"] == "":
             break
 
-        feed_dict["feed"]         = parse_resp.get("feed", "")
-        feed_dict["organization"] = parse_resp.get("organization", "")
-        feed_dict["website"]      = parse_resp.get("website", "")
-        feed_dict["schedule"]     = parse_resp.get("schedule", "")
-        feed_dict["updated_at"]   = parse_resp.get("stamp_pulled", "")
+        feed_dict["feed"]           = parse_resp.get("feed", "")
+        feed_dict["category"]       = parse_resp.get("category", "")
+        feed_dict["organization"]   = parse_resp.get("organization", "")
+        feed_dict["website"]        = parse_resp.get("website", "")
+        feed_dict["schedule"]       = parse_resp.get("schedule", "")
+        feed_dict["stamp added"]    = parse_resp.get("stamp_added", "")
+        feed_dict["stamp updated"]  = parse_resp.get("stamp_updated", "")
+        feed_dict["stamp pulled"]   = parse_resp.get("stamp_pulled", "")
+        feed_dict["stamp modified"] = parse_resp.get("stamp_modified", "")
 
         for key, value in feed_dict.iteritems():
             if value == None:
                 feed_dict[key] = ""
 
         feed_dicts.append(feed_dict)
-
         time.sleep(2)
 
     if len(feed_dicts) > 0:
@@ -95,10 +97,10 @@ def query_feeds(feed_dicts, session):
 
         for pr in parsed_resp:
             fdl = {}
-            fdl["indicator"]  = pr.get("indicator", "")
-            fdl["type"]       = pr.get("type", "")
-            fdl["risk"]       = pr.get("risk", "")
-            fdl["stamped_at"] = pr.get("stamp_linked", "")
+            fdl["indicator"]    = pr.get("indicator", "")
+            fdl["type"]         = pr.get("type", "")
+            fdl["risk"]         = pr.get("risk", "")
+            fdl["stamp linked"] = pr.get("stamp_linked", "")
             feed_dict = commons.merge_dict(fd, fdl)
 
             for key, value in feed_dict.iteritems():
@@ -151,25 +153,32 @@ def query_threats(session):
         if threat_dict["tid"] == "":
             continue
 
+        threat_dict["source"] = "threat"
         threat_dict["threat"] = parse_resp.get("threat", "")
         threat_dict["category"] = parse_resp.get("category", "")
         threat_dict["risk"] = parse_resp.get("risk", "")
         threat_dict["description"] = parse_resp.get("description", "")
-        threat_dict["wikisummary"] = parse_resp.get("wikisummary", "")
-        threat_dict["wikireference"] = parse_resp.get("wikireference", "")
+        threat_dict["wiki summary"] = parse_resp.get("wikisummary", "")
+        threat_dict["wiki reference"] = parse_resp.get("wikireference", "")
         threat_dict["retired"] = parse_resp.get("retired", "")
-        threat_dict["stamp_added"] = parse_resp.get("stamp_added", "")
-        threat_dict["stamp_updated"] = parse_resp.get("stamp_updated", "")
-        threat_dict["stamp_seen"] = parse_resp.get("stamp_seen", "")
-        threat_dict["stamp_retired"] = parse_resp.get("stamp_retired", "")
-        threat_dict["updated_last_domain"] = parse_resp.get("updated_last_domain", "")
-        threat_dict["othernames"] = "\n".join(parse_resp.get("othernames", ""))
-        threat_dict["techniques"] = "\n".join(parse_resp.get("techniques", ""))
-        threat_dict["news"] = "\n".join([x.get("link", "") for x in parse_resp["news"]])
+        threat_dict["stamp added"] = parse_resp.get("stamp_added", "")
+        threat_dict["stamp updated"] = parse_resp.get("stamp_updated", "")
+        threat_dict["stamp seen"] = parse_resp.get("stamp_seen", "")
+        threat_dict["stamp retired"] = parse_resp.get("stamp_retired", "")
+        threat_dict["updated last domain"] = parse_resp.get("updated_last_domain", "")
+        threat_dict["other names"] = "|".join(parse_resp.get("othernames", ""))
+        threat_dict["techniques"] = "|".join(parse_resp.get("techniques", ""))
+
+        if "news" in parse_resp:
+            threat_dict["news"] = "|".join([x.get("link", "") for x in parse_resp["news"]])
+        else:
+            threat_dict["news"] = ""
 
         for key, value in threat_dict.iteritems():
             if value == None:
                 threat_dict[key] = ""
+            else:
+                threat_dict[key] = value.replace(",", "")
 
         threat_dicts.append(threat_dict)
         time.sleep(2)
@@ -180,39 +189,28 @@ def query_threats(session):
         return threat_dicts
     return
 
-def write_file(feeds, threats, feed_path, threat_path):
+def write_file(data, file_path):
     """Write data to a file."""
-    if feeds == None:
-        feeds = []
+    if data != None:
+        with open(file_path, "w") as open_file:
+            keys   = data[0].keys()
+            header = ",".join(keys)
+            open_file.write("{}\n".format(header))
 
-    if threats == None:
-        threats = []
-
-    with open(feed_path, "w") as ffile, open(threat_path, "w") as tfile:
-        fkeys   = feeds[0].keys()
-        fheader = ",".join(fkeys)
-        ffile.write("{}\n".format(fheader))
-
-        for feed in feeds:
-            feed_string = ",".join(feed.values())
-            ffile.write("{}\n".format(feed_string.encode("UTF-8")))
-
-        tkeys   = threats[0].keys()
-        theader = ",".join(tkeys)
-        tfile.write("{}\n".format(theader))
-
-        for threat in threats:
-            threat_string = ",".join(threat.values())
-            tfile.write("{}\n".format(threat_string.encode("UTF-8")))
+            for obj in data:
+                obj_string = ",".join(obj.values())
+                open_file.write("{}\n".format(obj_string.encode("UTF-8")))
     return
 
 if __name__ == "__main__":
     if sys.argv[1].lower() == "feed":
         feeds, threats = get_feed()
         lookup_path    = "/opt/splunk/etc/apps/osweep/lookups"
-        feed_path      = "{}/pulsedive_feed.csv".format(lookup_path)
-        threat_path    = "{}/pulsedive_threat.csv".format(lookup_path)
-        write_file(feeds, threats, feed_path, threat_path)
+
+        for data in [feeds, threats]:
+            source    = data[0]["source"]
+            file_path = "{}/pulsedive_{}.csv".format(lookup_path, source)
+            write_file(data, file_path)
         exit(0)
 
     current_module = sys.modules[__name__]
