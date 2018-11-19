4275bbe7e760d649e0932846fb2bd5f1d1e96878
ecstatic-nobel <theemailaddressishare@protonmail.com>
2018-11-19 00:03:28 -0500

    Import feed and threat names from module

--- bin/pulsedive.py
+++ bin/pulsedive.py
@@ -11,6 +11,7 @@ script_path = os.path.dirname(os.path.realpath(__file__)) + "/_tp_modules"
 sys.path.insert(0, script_path)
 
 import commons
+import pulsedive_names
 
 
 info_api  = "https://pulsedive.com/api/info.php"
@@ -33,15 +34,10 @@ def get_feed():
 def query_list(session):
     """Return a list of feed IDs."""
     feed_dicts = []
-    fids = [
-        1, 10, 11, 13, 14, 17, 18, 19, 2, 20, 22, 23, 24, 27, 28, 29, 3, 31, 32, 
-        33, 34, 35, 36, 37, 39, 4, 41, 42, 44, 45, 47, 48, 49, 5, 51, 53, 55, 
-        56, 57, 58, 59, 6, 60, 61, 62, 7, 8, 9
-    ]
 
-    for feed_id in fids:
+    for feed in pulsedive_names.feed:
         data = {
-        "fid": str(feed_id),
+        "feed": str(feed),
         "key": api_key
         }
         resp = session.get(info_api, params=data)
@@ -50,13 +46,9 @@ def query_list(session):
             continue
 
         parse_resp = resp.json()
-        feed_dict           = {}
-        feed_dict["source"] = "feed"
-        feed_dict["fid"]    = str(parse_resp.get("fid", ""))
-
-        if feed_dict["fid"] == "":
-            break
-
+        feed_dict = {}
+        feed_dict["source"]         = "feed"
+        feed_dict["fid"]            = str(parse_resp.get("fid", ""))
         feed_dict["feed"]           = parse_resp.get("feed", "")
         feed_dict["category"]       = parse_resp.get("category", "")
         feed_dict["organization"]   = parse_resp.get("organization", "")
@@ -117,28 +109,10 @@ def query_feeds(feed_dicts, session):
 def query_threats(session):
     """Return data about each threat."""
     threat_dicts = []
-    tids = [
-        1, 10, 100, 101, 103, 106, 107, 108, 109, 11, 110, 111, 112, 113, 114, 
-        117, 119, 12, 120, 121, 122, 123, 124, 126, 127, 128, 129, 13, 130, 131, 
-        132, 133, 134, 135, 136, 138, 14, 140, 141, 143, 147, 148, 149, 15, 150, 
-        153, 154, 156, 157, 158, 159, 16, 160, 161, 162, 163, 164, 165, 166, 167, 
-        168, 169, 17, 170, 171, 172, 173, 174, 175, 177, 179, 18, 180, 182, 183, 
-        184, 185, 186, 187, 188, 189, 19, 190, 191, 192, 193, 194, 195, 196, 197, 
-        198, 199, 20, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 21, 210, 
-        212, 213, 214, 215, 216, 217, 218, 219, 22, 220, 221, 222, 223, 224, 225, 
-        227, 228, 229, 23, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 
-        241, 242, 243, 244, 245, 246, 247, 248, 249, 25, 250, 251, 252, 253, 254, 
-        255, 256, 257, 258, 259, 26, 260, 261, 262, 263, 264, 265, 266, 267, 268, 
-        269, 27, 270, 271, 272, 273, 274, 275, 276, 277, 278, 279, 28, 280, 281, 
-        282, 283, 284, 29, 3, 30, 32, 35, 36, 38, 39, 4, 40, 41, 42, 43, 44, 45, 
-        46, 47, 48, 49, 5, 50, 51, 52, 53, 54, 56, 57, 58, 59, 6, 60, 61, 62, 63, 
-        65, 66, 67, 68, 69, 7, 70, 71, 72, 73, 75, 76, 81, 83, 84, 85, 87, 89, 9, 
-        90, 91, 92, 94, 95, 96, 97, 99
-    ]
 
-    for threat_id in tids:
+    for threat in pulsedive_names.threat:
         data = {
-        "tid": str(threat_id),
+        "threat": str(threat),
         "key": api_key
         }
         resp = session.get(info_api, params=data)
@@ -146,28 +120,24 @@ def query_threats(session):
         if resp.status_code != 200 or "error" in resp.json().keys():
             continue
 
-        parse_resp         = resp.json()
-        threat_dict        = {}
-        threat_dict["tid"] = str(parse_resp.get("tid", ""))
-
-        if threat_dict["tid"] == "":
-            continue
-
-        threat_dict["source"] = "threat"
-        threat_dict["threat"] = parse_resp.get("threat", "")
-        threat_dict["category"] = parse_resp.get("category", "")
-        threat_dict["risk"] = parse_resp.get("risk", "")
-        threat_dict["description"] = parse_resp.get("description", "")
-        threat_dict["wiki summary"] = parse_resp.get("wikisummary", "")
-        threat_dict["wiki reference"] = parse_resp.get("wikireference", "")
-        threat_dict["retired"] = parse_resp.get("retired", "")
-        threat_dict["stamp added"] = parse_resp.get("stamp_added", "")
-        threat_dict["stamp updated"] = parse_resp.get("stamp_updated", "")
-        threat_dict["stamp seen"] = parse_resp.get("stamp_seen", "")
-        threat_dict["stamp retired"] = parse_resp.get("stamp_retired", "")
+        parse_resp  = resp.json()
+        threat_dict = {}
+        threat_dict["source"]              = "threat"
+        threat_dict["tid"]                 = str(parse_resp.get("tid", ""))
+        threat_dict["threat"]              = parse_resp.get("threat", "")
+        threat_dict["category"]            = parse_resp.get("category", "")
+        threat_dict["risk"]                = parse_resp.get("risk", "")
+        threat_dict["description"]         = parse_resp.get("description", "")
+        threat_dict["wiki summary"]        = parse_resp.get("wikisummary", "")
+        threat_dict["wiki reference"]      = parse_resp.get("wikireference", "")
+        threat_dict["retired"]             = parse_resp.get("retired", "")
+        threat_dict["stamp added"]         = parse_resp.get("stamp_added", "")
+        threat_dict["stamp updated"]       = parse_resp.get("stamp_updated", "")
+        threat_dict["stamp seen"]          = parse_resp.get("stamp_seen", "")
+        threat_dict["stamp retired"]       = parse_resp.get("stamp_retired", "")
         threat_dict["updated last domain"] = parse_resp.get("updated_last_domain", "")
-        threat_dict["other names"] = "|".join(parse_resp.get("othernames", ""))
-        threat_dict["techniques"] = "|".join(parse_resp.get("techniques", ""))
+        threat_dict["other names"]         = "|".join(parse_resp.get("othernames", ""))
+        threat_dict["techniques"]          = "|".join(parse_resp.get("techniques", ""))
 
         if "news" in parse_resp:
             threat_dict["news"] = "|".join([x.get("link", "") for x in parse_resp["news"]])
