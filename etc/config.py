80d91d4bd99708c85c7663b4587ce9513051b129
ecstatic-nobel <theemailaddressishare@protonmail.com>
2018-11-23 00:08:40 -0500

    Move Pulsedive dashboard development to pulsedive branch

--- etc/config.py
+++ etc/config.py
@@ -11,9 +11,6 @@ https_proxy_url = None
 # GreyNoise API
 greynoise_key = None
 
-# Pulsedive API
-pulsedive_apikey = None
-
 # Twitter API
 twitter_consumer_key        = None
 twitter_consumer_secret     = None
